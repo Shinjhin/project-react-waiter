dec86c6c7ddbf5a9f4e9e49fb9a1ea9b39ada6de
Shinjhin <huberthubka199@gmail.com>
2022-03-08 23:21:24 +0100

    add basic tables and layout for main.js

--- src/components/pages/main/main.js
+++ src/components/pages/main/main.js
@@ -1,9 +1,16 @@
+import Table from "../../views/table/table";
+import styles from './main.module.scss'
+
 const Main = () => {
     return(
         <div>
             <p>Witamy na głównej stronie!</p>
+            <h2 className={styles.title}>All tables</h2>
+            <Table />
+            <Table />
+            <Table />
+            <Table />
         </div>
     )
 }
-
 export default Main;
